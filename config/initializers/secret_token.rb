513b0084f1f0d427938c02e902be2250d04dee79
Christopher Turtle <hosmerangel@gmail.com>
2011-01-29 18:39:47 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camfort::Application.config.secret_token = '4965e5f02545ba39926fbc09eb227ce0dcc26fb4368a4cedbacb33107674773da4a1170216a2fbc1607faf542e28ce8e189f633e4cd9df76ce0767729343b276'
